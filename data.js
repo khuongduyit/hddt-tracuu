c8a586ba16f35bacbc72c2c94723666b18a2a0d6
khuongduyit <dangkhuongduytn@gmail.com>
2019-08-27 13:54:55 +0700

    Fix conflict

--- data.js
+++ data.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 var data = [
 	{"name":"Phiên bản của hóa đơn", "code":"PBan"},
 	{"name":"Ký hiệu mẫu số hóa đơn", "code":"KHMSHDon"}
-=======
-var data = [
-	{"name":"Phiên bản của hóa đơn", "code":"PBan"},
-	{"name":"Ký hiệu mẫu số hóa đơn", "code":"KHMSHDon"}
->>>>>>> f1523f29f7291e9639fbca10ec81511bff8b334a
 ];
