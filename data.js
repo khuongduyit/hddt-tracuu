6c9141bd04daf14f1183bf02dafd69f8fda7a9ca
khuongduyit <dangkhuongduytn@gmail.com>
2019-08-27 14:04:12 +0700

    update data

--- data.js
+++ data.js
@@ -1,4 +1,198 @@
 var data = [
-	{"name":"Phiên bản của hóa đơn", "code":"PBan"},
-	{"name":"Ký hiệu mẫu số hóa đơn", "code":"KHMSHDon"}
+  {
+    "name": "Phiên bản của hóa đơn",
+    "code": "PBan"
+  },
+  {
+    "name": "Tên hóa đơn",
+    "code": "THDon"
+  },
+  {
+    "name": "Ký hiệu mẫu số hóa đơn",
+    "code": "KHMSHDon"
+  },
+  {
+    "name": "Ký hiệu hóa đơn",
+    "code": "KHHDon"
+  },
+  {
+    "name": "Số hóa đơn",
+    "code": "SHDon"
+  },
+  {
+    "name": "Thời điểm lập",
+    "code": "TDLap"
+  },
+  {
+    "name": "Tính chất của hóa đơn",
+    "code": "TChat"
+  },
+  {
+    "name": "Đơn vị tiền tệ",
+    "code": "DVTTe"
+  },
+  {
+    "name": "Tỷ giá",
+    "code": "TGia"
+  },
+  {
+    "name": "Tên trường",
+    "code": "TTruong"
+  },
+  {
+    "name": "Kiểu dữ liệu",
+    "code": "KDLieu"
+  },
+  {
+    "name": "Dữ liệu",
+    "code": "DLieu"
+  },
+  {
+    "name": "Họ tên người bán hàng",
+    "code": "Ten"
+  },
+  {
+    "name": "Mã số thuế người bán",
+    "code": "MST"
+  },
+  {
+    "name": "Địa chỉ người bán",
+    "code": "DChi"
+  },
+  {
+    "name": "Họ tên người mua",
+    "code": "Ten"
+  },
+  {
+    "name": "Mã số thuế người mua",
+    "code": "MST"
+  },
+  {
+    "name": "Địa chỉ người mua",
+    "code": "DChi"
+  },
+  {
+    "name": "Tính chất hàng hóa",
+    "code": "TCHHoa"
+  },
+  {
+    "name": "Số thứ tự",
+    "code": "STT"
+  },
+  {
+    "name": "Tên hàng hóa dịch vụ",
+    "code": "Ten"
+  },
+  {
+    "name": "Đơn vị tính",
+    "code": "DVTinh"
+  },
+  {
+    "name": "Số lượng hàng hóa - dịch vụ",
+    "code": "SLuong"
+  },
+  {
+    "name": "Đơn giá",
+    "code": "DGia"
+  },
+  {
+    "name": "Thành tiền chưa có thuế GTGT",
+    "code": "ThTien"
+  },
+  {
+    "name": "Thuế suất thuế GTGT",
+    "code": "TSuat"
+  },
+  {
+    "name": "Thuế suất",
+    "code": "TSuat"
+  },
+  {
+    "name": "Tiền thuế GTGT",
+    "code": "TThue"
+  },
+  {
+    "name": "Thành tiền chưa thuế",
+    "code": "ThTien"
+  },
+  {
+    "name": "Tổng cộng thành tiền chưa có thuế VAT",
+    "code": "TgTCThue"
+  },
+  {
+    "name": "Tổng cộng tiền thuế",
+    "code": "TgTThue"
+  },
+  {
+    "name": "Tên loại phí",
+    "code": "TLPhi"
+  },
+  {
+    "name": "Tiền phí",
+    "code": "TPhi"
+  },
+  {
+    "name": "Tổng số tiền thanh toán bằng số",
+    "code": "TgTTTBSo"
+  },
+  {
+    "name": "Tổng số tiền thanh toán bằng chữ",
+    "code": "TgTTTBChu"
+  },
+  {
+    "name": "Tổng số tiền thanh toán bằng số (ghi bằng Đồng Việt Nam)",
+    "code": "TgTTTVNDBSo"
+  },
+  {
+    "name": "Tổng số tiền thanh toán bằng chữ (ghi bằng Đồng Việt Nam)",
+    "code": "TgTTTVNDBChu"
+  },
+  {
+    "name": "Danh sách chữ ký số khác (do DN tự định nghĩa - ví dụ DN có nhu cầu ký số nội bộ)",
+    "code": "CCKSKhac"
+  },
+  {
+    "name": "Hình thức hóa đơn bị thay thế",
+    "code": "HTHDBTThe"
+  },
+  {
+    "name": "Thời điểm lập hóa đơn bị thay thế",
+    "code": "TDLHDBTThe"
+  },
+  {
+    "name": "Ký hiệu mẫu số hóa đơn bị thay thế",
+    "code": "KHMSHDBTThe"
+  },
+  {
+    "name": "Ký hiệu hóa đơn bị thay thế",
+    "code": "KHHDBTThe"
+  },
+  {
+    "name": "Số hóa đơn bị thay thế",
+    "code": "SHDBTThe"
+  },
+  {
+    "name": "Mã của CQT",
+    "code": "MCCQT"
+  },
+  {
+    "name": "MST người bán (của hóa đơn cần cấp mã của CQT)",
+    "code": "MSTNBan"
+  },
+  {
+    "name": "Ký hiệu mẫu số hóa đơn (của hóa đơn cần cấp mã của CQT)",
+    "code": "KHMSHDon"
+  },
+  {
+    "name": "Ký hiệu hóa đơn (của hóa đơn cần cấp mã của CQT)",
+    "code": "KHHDon"
+  },
+  {
+    "name": "Số hóa đơn (của hóa đơn cần cấp mã của CQT)",
+    "code": "SHDon"
+  },
+  {
+    "name": "Thời điểm lập (của hóa đơn cần cấp mã của CQT)",
+    "code": "TDLap"
+  }
 ];
