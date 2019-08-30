666903bd185e4740962d0fed2062fdf4f2d67ba9
khuongduyit <dangkhuongduytn@gmail.com>
2019-08-30 16:30:36 +0700

    Convert to table

--- data.js
+++ data.js
@@ -1,4 +1,128 @@
-var data = [
+var json_data = [
+  {
+    "name": "Chữ ký số",
+    "code": "CKS"
+  },
+  {
+    "name": "Mã số thuế",
+    "code": "MST"
+  },
+  {
+    "name": "Tổng cục Thuế",
+    "code": "TCT"
+  },
+  {
+    "name": "Cơ quan Thuế",
+    "code": "CQT"
+  },
+  {
+    "name": "Số thứ tự",
+    "code": "STT"
+  },
+  {
+    "name": "Người nộp thuế",
+    "code": "NNT"
+  },
+  {
+    "name": "Hoá đơn",
+    "code": "HDon"
+  },
+  {
+    "name": "Dữ liệu hoá đơn",
+    "code": "DLHDon"
+  },
+  {
+    "name": "Thông tin cơ quan thuế",
+    "code": "TTCQT"
+  },
+  {
+    "name": "Danh sách chữ ký số",
+    "code": "DSCKS"
+  },
+  {
+    "name": "Thông tin khác",
+    "code": "TTKhac"
+  },
+  {
+    "name": "Thông tin",
+    "code": "TTin"
+  },
+  {
+    "name": "Tên trường thông tin cần hiển thị trên HĐ",
+    "code": "TTruong"
+  },
+  {
+    "name": "Kiểu dữ liệu cần hiển thị trên HĐ",
+    "code": "KDLieu"
+  },
+  {
+    "name": "Dữ liệu cần hiển thị",
+    "code": "DLieu"
+  },
+  {
+    "name": "Hóa đơn điện tử",
+    "code": "HDDT"
+  },
+  {
+    "name": "Đăng ký thuế",
+    "code": "ĐKT"
+  },
+  {
+    "name": "Bộ Tài chính",
+    "code": "BTC"
+  },
+  {
+    "name": "Doanh nghiệp lớn",
+    "code": "DNL"
+  },
+  {
+    "name": "Extensible Markup Language",
+    "code": "XML"
+  },
+  {
+    "name": "Tổ chức cung cấp dịch vụ về hóa đơn điện tử đã ký hợp đồng với Tổng cục Thuế",
+    "code": "T-VAN"
+  },
+  {
+    "name": "Mã nơi gửi là MST của người gửi",
+    "code": "MNGui"
+  },
+  {
+    "name": "Mã nơi nhận là MST của người nhận",
+    "code": "MNNhan"
+  },
+  {
+    "name": "Thông điệp",
+    "code": "TDiep"
+  },
+  {
+    "name": "Thông tin chung",
+    "code": "TTChung"
+  },
+  {
+    "name": "Phiên bản của thông điệp",
+    "code": "PBan"
+  },
+  {
+    "name": "Mã nơi gửi",
+    "code": "MNGui"
+  },
+  {
+    "name": "Mã nơi nhận",
+    "code": "MNNhan"
+  },
+  {
+    "name": "Mã loại giao dịch. Tham chiếu trong danh sách các loại thông điệp truyền nhận",
+    "code": "MLGDich"
+  },
+  {
+    "name": "Mã giao dịch",
+    "code": "MGDich"
+  },
+  {
+    "name": "Mã giao dịch tham chiếu (là giá trị mã giao dịch gốc trong trường hợp là thông điệp phản hồi kết quả)",
+    "code": "MGDTChieu"
+  },
   {
     "name": "Phiên bản của hóa đơn",
     "code": "PBan"
@@ -36,18 +160,6 @@ var data = [
     "code": "TGia"
   },
   {
-    "name": "Tên trường",
-    "code": "TTruong"
-  },
-  {
-    "name": "Kiểu dữ liệu",
-    "code": "KDLieu"
-  },
-  {
-    "name": "Dữ liệu",
-    "code": "DLieu"
-  },
-  {
     "name": "Họ tên người bán hàng",
     "code": "Ten"
   },
@@ -58,6 +170,14 @@ var data = [
   {
     "name": "Địa chỉ người bán",
     "code": "DChi"
+  },
+  {
+    "name": "Nội dung hoá đơn",
+    "code": "NDHDon"
+  },
+  {
+    "name": "Người mua",
+    "code": "NMua"
   },
   {
     "name": "Họ tên người mua",
@@ -72,12 +192,16 @@ var data = [
     "code": "DChi"
   },
   {
-    "name": "Tính chất hàng hóa",
-    "code": "TCHHoa"
+    "name": "Danh sách hàng hoá dịch vụ",
+    "code": "DSHHDVu"
   },
   {
-    "name": "Số thứ tự",
-    "code": "STT"
+    "name": "Hàng hoá dịch vụ",
+    "code": "HHDVu"
+  },
+  {
+    "name": "Tính chất hàng hóa",
+    "code": "TCHHoa"
   },
   {
     "name": "Tên hàng hóa dịch vụ",
@@ -104,6 +228,10 @@ var data = [
     "code": "TSuat"
   },
   {
+    "name": "Tổng hợp theo từng loại thuế suất",
+    "code": "THTTLTSuat"
+  },
+  {
     "name": "Thuế suất",
     "code": "TSuat"
   },
@@ -114,6 +242,10 @@ var data = [
   {
     "name": "Thành tiền chưa thuế",
     "code": "ThTien"
+  },
+  {
+    "name": "Thanh toán",
+    "code": "TToan"
   },
   {
     "name": "Tổng cộng thành tiền chưa có thuế VAT",
@@ -146,10 +278,6 @@ var data = [
   {
     "name": "Tổng số tiền thanh toán bằng chữ (ghi bằng Đồng Việt Nam)",
     "code": "TgTTTVNDBChu"
-  },
-  {
-    "name": "Danh sách chữ ký số khác (do DN tự định nghĩa - ví dụ DN có nhu cầu ký số nội bộ)",
-    "code": "CCKSKhac"
   },
   {
     "name": "Hình thức hóa đơn bị thay thế",
@@ -194,5 +322,397 @@ var data = [
   {
     "name": "Thời điểm lập (của hóa đơn cần cấp mã của CQT)",
     "code": "TDLap"
+  },
+  {
+    "name": "Họ tên người bán hàng (người xuất hàng hóa)",
+    "code": "Ten"
+  },
+  {
+    "name": "Địa chỉ người bán (địa điểm kho xuất hàng)",
+    "code": "DChi"
+  },
+  {
+    "name": "Thông tin lệnh điều động nội bộ",
+    "code": "TTLDDong"
+  },
+  {
+    "name": "Tên người vận chuyển",
+    "code": "TNVChuyen"
+  },
+  {
+    "name": "Phương tiện vận chuyển",
+    "code": "PTVChuyen"
+  },
+  {
+    "name": "Số hộ chiếu/Giấy tờ nhập cảnh",
+    "code": "SHChieu"
+  },
+  {
+    "name": "Quốc tịch",
+    "code": "QTich"
+  },
+  {
+    "name": "Mã tờ khai",
+    "code": "MTKhai"
+  },
+  {
+    "name": "Tên tờ khai",
+    "code": "TTKhai"
+  },
+  {
+    "name": "Loại kỳ tính thuế",
+    "code": "LKTThue"
+  },
+  {
+    "name": "Kỳ tính thuế",
+    "code": "KTThue"
+  },
+  {
+    "name": "Địa chỉ NNT",
+    "code": "DCNNT"
+  },
+  {
+    "name": "Tên NNT",
+    "code": "TNNT"
+  },
+  {
+    "name": "Mã số thuế NNT",
+    "code": "MSTNNT"
+  },
+  {
+    "name": "Tên đại lý thuế",
+    "code": "TDLThue"
+  },
+  {
+    "name": "Mã số thuế đại lý thuế",
+    "code": "MSTDLThue"
+  },
+  {
+    "name": "Nhóm hoá đơn không chịu thuế",
+    "code": "NHDKCThue"
+  },
+  {
+    "name": "Danh sách hoá đơn không chịu thuế",
+    "code": "DSHDKCThue"
+  },
+  {
+    "name": "Thời điểm lập hóa đơn",
+    "code": "TDLap"
+  },
+  {
+    "name": "Tên người mua",
+    "code": "TNMua"
+  },
+  {
+    "name": "Mã số thuế người mua",
+    "code": "MSTNMua"
+  },
+  {
+    "name": "Doanh thu chưa có thuế GTGT",
+    "code": "DTCThue"
+  },
+  {
+    "name": "Ghi chú",
+    "code": "GChu"
+  },
+  {
+    "name": "Tổng doanh thu chưa có thuế GTGT",
+    "code": "TgDTCThue"
+  },
+  {
+    "name": "Tổng thuế GTGT",
+    "code": "TgTGTGT"
+  },
+  {
+    "name": "Nhóm hoá đơn thuế không chịu thuế 0%",
+    "code": "NHDThue0"
+  },
+  {
+    "name": "Nhóm hoá đơn thuế không chịu thuế 5%",
+    "code": "NHDThue5"
+  },
+  {
+    "name": "Nhóm hoá đơn thuế không chịu thuế 10%",
+    "code": "NHDThue10"
+  },
+  {
+    "name": "Tổng doanh thu hàng hóa - dịch vụ bán ra chịu thuế GTGT",
+    "code": "TgDThu"
+  },
+  {
+    "name": "Phiên bản",
+    "code": "PBan"
+  },
+  {
+    "name": "Người liên hệ",
+    "code": "NLHe"
+  },
+  {
+    "name": "Địa chỉ liên hệ",
+    "code": "DCLHe"
+  },
+  {
+    "name": "Địa chỉ thư điện tử",
+    "code": "DCTDTu"
+  },
+  {
+    "name": "Điện thoại liên hệ",
+    "code": "DTLHe"
+  },
+  {
+    "name": "Nơi lập",
+    "code": "NLap"
+  },
+  {
+    "name": "Áp dụng HDDT (có mã/ không mã)",
+    "code": "ADHDDT"
+  },
+  {
+    "name": "Đăng ký giao dịch qua",
+    "code": "DKGDQua"
+  },
+  {
+    "name": "Tờ khai",
+    "code": "TKhai"
+  },
+  {
+    "name": "Dữ liệu tờ khai",
+    "code": "DLTKhai"
+  },
+  {
+    "name": "Nội dung tờ khai",
+    "code": "NDTKhai"
+  },
+  {
+    "name": "Danh sách loại hoá đơn sử dụng",
+    "code": "DSLHDSDung"
+  },
+  {
+    "name": "Loại hoá đơn sử dụng",
+    "code": "LHDSDung"
+  },
+  {
+    "name": "Mã loại HĐ sử dụng",
+    "code": "MLHDSDung"
+  },
+  {
+    "name": "Danh sách chứng thư số sử dụng",
+    "code": "DSCTSSDung"
+  },
+  {
+    "name": "Chứng thư số sử dụng",
+    "code": "CTSSDung"
+  },
+  {
+    "name": "Tên tổ chức cơ quan chứng thực/cấp/công nhận chữ ký số - chữ ký điện tử",
+    "code": "TTChuc"
+  },
+  {
+    "name": "Số sê-ri chứng thư số",
+    "code": "Seri"
+  },
+  {
+    "name": "Thời hạn sử dụng từ ngày",
+    "code": "TNgay"
+  },
+  {
+    "name": "Thời hạn sử dụng đến ngày",
+    "code": "DNgay"
+  },
+  {
+    "name": "Hình thức đăng ký",
+    "code": "HTDKy"
+  },
+  {
+    "name": "Dữ liệu tờ khai",
+    "code": "DLTBao"
+  },
+  {
+    "name": "Phiên bản XML",
+    "code": "PBan"
+  },
+  {
+    "name": "Mã CQT cấp trên",
+    "code": "MCQTCTren"
+  },
+  {
+    "name": "Tên CQT cấp trên",
+    "code": "TCQTCTren"
+  },
+  {
+    "name": "Mã CQT",
+    "code": "MCQT"
+  },
+  {
+    "name": "Tên CQT",
+    "code": "TCQT"
+  },
+  {
+    "name": "Số thông báo CQT",
+    "code": "So"
+  },
+  {
+    "name": "Mã thông báo CQT",
+    "code": "Ma"
+  },
+  {
+    "name": "Mẫu thông báo CQT",
+    "code": "Mau"
+  },
+  {
+    "name": "Tên thông báo CQT",
+    "code": "Ten"
+  },
+  {
+    "name": "Địa chỉ CQT",
+    "code": "DCCQT"
+  },
+  {
+    "name": "Ngày thông báo",
+    "code": "Ngay"
+  },
+  {
+    "name": "Loại đăng ký",
+    "code": "LDKy"
+  },
+  {
+    "name": "Ngày đăng ký",
+    "code": "NDKy"
+  },
+  {
+    "name": "Trạng thái xác nhận của CQT",
+    "code": "TTXNTCT"
+  },
+  {
+    "name": "Lý do",
+    "code": "LDo"
+  },
+  {
+    "name": "Trưởng phòng cơ quan thuế",
+    "code": "TPCQT"
+  },
+  {
+    "name": "Thủ trưởng cơ quan thuế",
+    "code": "TTCQT"
+  },
+  {
+    "name": "Tên tổ chức - cá nhân",
+    "code": "TTCCNhan"
+  },
+  {
+    "name": "Số quyết định thành lập tổ chức",
+    "code": "SQDTLap"
+  },
+  {
+    "name": "Tên người nhận hóa đơn",
+    "code": "TNNHDon"
+  },
+  {
+    "name": "Số CMND người nhận HĐ",
+    "code": "SCMNDNNhan"
+  },
+  {
+    "name": "Nơi cấp CMND người nhận HĐ",
+    "code": "NCCMNDNNhan"
+  },
+  {
+    "name": "Số - ngày hợp đồng mua bán",
+    "code": "SNHDong"
+  },
+  {
+    "name": "Doanh thu phát sinh",
+    "code": "DTPSinh"
+  },
+  {
+    "name": "Mã lỗi",
+    "code": "MLoi"
+  },
+  {
+    "name": "Mô tả",
+    "code": "MTa"
+  },
+  {
+    "name": "Phiên bản của thông báo",
+    "code": "PBan"
+  },
+  {
+    "name": "Tên người nộp thuế",
+    "code": "TNNT"
+  },
+  {
+    "name": "Mã số thuế NNT",
+    "code": "MST"
+  },
+  {
+    "name": "Ngày thông báo",
+    "code": "NTBao"
+  },
+  {
+    "name": "Ngày yêu cầu chuyển đổi",
+    "code": "NYCCDoi"
+  },
+  {
+    "name": "Số bảng tổng hợp dữ liệu",
+    "code": "SBTHDLieu"
+  },
+  {
+    "name": "Loại kỳ dữ liệu",
+    "code": "LKDLieu"
+  },
+  {
+    "name": "Kỳ dữ liệu",
+    "code": "KDLieu"
+  },
+  {
+    "name": "Lần đầu",
+    "code": "LDau"
+  },
+  {
+    "name": "Bổ sung lần thứ",
+    "code": "BSLThu"
+  },
+  {
+    "name": "Bảng tổng hợp",
+    "code": "BTHop"
+  },
+  {
+    "name": "Bảng tổng hợp dữ liệu",
+    "code": "BTHDLieu"
+  },
+  {
+    "name": "Nội dung bảng tổng hợp dữ liệu",
+    "code": "NDBTHDLieu"
+  },
+  {
+    "name": "Mã số thuế người mua/mã khách hàng",
+    "code": "MSTNMua"
+  },
+  {
+    "name": "Tên hàng hóa - dịch vụ",
+    "code": "THHDVu"
+  },
+  {
+    "name": "Số lượng hàng hóa",
+    "code": "SLuong"
+  },
+  {
+    "name": "Tổng giá trị hàng hoá - dịch vụ bán ra chưa có thuế GTGT",
+    "code": "TTCThue"
+  },
+  {
+    "name": "Thuế suất thuế GTGT",
+    "code": "TSTGTGT"
+  },
+  {
+    "name": "Tổng số thuế GTGT",
+    "code": "TgTThue"
+  },
+  {
+    "name": "Tổng tiền thanh toán",
+    "code": "TgTTToan"
+  },
+  {
+    "name": "Gateway",
+    "code": "GW"
   }
 ];
